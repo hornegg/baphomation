4a831707562701d4ce5b46554ef5d3412b5c09da
hornegg <hornegg@users.noreply.github.com>
2021-03-27 20:55:01 +0000

    Clean up the cylinders that I'm no longer trying to draw the wing with.

--- model/bodyGeometry.ts
+++ model/bodyGeometry.ts
@@ -3,7 +3,6 @@ import * as THREE from 'three';
 import * as bspConstructor from 'three-js-csg';
 
 import { createEllipsoid } from './commonGeometry';
-import { HALF_PI } from '../src/common';
 
 const ThreeBSP = bspConstructor(THREE);
 
@@ -12,37 +11,11 @@ const outline = process.argv[3] === 'true';
 
 const scalar = outline ? 0.07 : 0;
 
-interface CylinderParams {
-  sign: 1 | -1;
-  x: number;
-  y: number;
-  z: number;
-  width: number;
-  height: number;
-  depth: number;
-}
+const createWing = (sign: 1 | -1) => {
+  const bladeEndX = 1.94;
+  const bladeEndY = -0.28;
+  const drop = 0.5;
 
-const createCylinder = (params: CylinderParams) => {
-  const radialSegments = 16;
-  const cylinder = new THREE.CylinderGeometry(
-    params.width + scalar,
-    params.height + scalar,
-    params.depth + scalar,
-    radialSegments
-  );
-
-  cylinder.rotateX(HALF_PI);
-  cylinder.translate(params.sign * params.x, params.y, params.z);
-
-  return new ThreeBSP(cylinder);
-};
-
-const createWingBlade = (
-  sign: 1 | -1,
-  bladeEndX: number,
-  bladeEndY: number,
-  drop: number
-) => {
   const quadParams = (
     p1: [number, number],
     p2: [number, number]
@@ -87,46 +60,6 @@ const createWingBlade = (
 
   const geom = new THREE.ExtrudeGeometry(shape, extrudeSettings);
   return new ThreeBSP(geom);
-};
-
-const createWing = (sign: 1 | -1) => {
-  const size = { width: 0.35, height: 0.35, depth: 0.05 };
-
-  const pos1 = { x: 1.1, y: -1.4, z: -0.25 /*0*/ };
-
-  const pos2 = {
-    x: pos1.x + (1.4 * size.width),
-    y: pos1.y + (0.8 * size.height),
-    z: pos1.z,
-  };
-
-  const pos3 = {
-    x: pos2.x + (1.0 * size.width),
-    y: pos2.y + (1.4 * size.height),
-    z: pos2.z,
-  };
-
-  const c1 = createCylinder({
-    sign,
-    ...pos1,
-    ...size,
-  });
-
-  const c2 = createCylinder({
-    sign,
-    ...pos2,
-    ...size,
-  });
-
-  const c3 = createCylinder({
-    sign,
-    ...pos3,
-    ...size,
-  });
-
-  const blade = createWingBlade(sign, pos3.x, pos3.y + size.height, 0.5);
-  return blade;
-  return c1.union(c2).union(c3).union(blade);
 };
 
 const bodyEllipsoid = createEllipsoid(0.75, 1.8, 0.5, scalar);
