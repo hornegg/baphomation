680cf596d17f3e9392f51b0f3b956a3f20cf7533
hornegg <hornegg@users.noreply.github.com>
2021-04-05 10:34:58 +0100

    Fix typo, and run a couple of automated processes I wasn't up to date with.

--- src/Room.tsx
+++ src/Room.tsx
@@ -41,9 +41,9 @@ const floorThickness = 0.1;
 const floor = createOutlinedBox(5, floorThickness, 5, 0.15, floorMaterial);
 floor.position.set(0, floorLevel - floorThickness - floorThickness, 0);
 
-const alterHeight = 1.25;
-const altar = createOutlinedBox(0.7, alterHeight, 1.25, 0.15, altarMaterial);
-altar.position.set(-2.2, floorLevel + (0.5 * alterHeight) + floorThickness, 0);
+const altarHeight = 1.25;
+const altar = createOutlinedBox(0.7, altarHeight, 1.25, 0.15, altarMaterial);
+altar.position.set(-2.2, floorLevel + (0.5 * altarHeight) + floorThickness, 0);
 
 const Room = (): JSX.Element => (
   <group>
